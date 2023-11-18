4dafbabee82cd68fa218e449856ad9d226a938c9
sv <bublikovsv47@gmail.com>
2023-11-18 21:06:49 +0300

    add key

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-$(document).ready(function() {
-    $("#dynamic-text").html("text");
-  });
-  
+
+
+const apiKey = process.env.ALCH_KEY
+
+const url = "https://eth-mainnet.g.alchemy.com/v2/"+apiKey;
+const address = "0x0177f45d3d6cBC133f81FC876be0aD642e1b44cb";
+
+fetch(`${url}/getAssetTransfers?address=${address}`)
+ .then(response => response.json())
+ .then(data => {
+  const element = document.getElementById('output');
+  element.textContent = JSON.stringify(data);
+ })
+ .catch(error => console.error('Error:', error));
