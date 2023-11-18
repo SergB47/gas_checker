4f537336b32fb9b8c805308901ec2e137ac8f3de
sv <bublikovsv47@gmail.com>
2023-11-18 23:01:47 +0300

    add key

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 
-const apiKey = process.env.ALCH_KEY
 
-const url = "https://eth-mainnet.g.alchemy.com/v2/"+apiKey;
+const url = "https://eth-mainnet.g.alchemy.com/v2/AWfFA3kSRIamfM4kBXR9dwejM-9vcOtc";
 const address = "0x0177f45d3d6cBC133f81FC876be0aD642e1b44cb";
 
 fetch(`${url}/getAssetTransfers?address=${address}`)
